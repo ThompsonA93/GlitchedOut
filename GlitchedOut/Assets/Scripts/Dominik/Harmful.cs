f49c0d3e4c559aa424add107e07842e31fe176c7
chiptuny22 <121547723+chiptuny22@users.noreply.github.com>
2025-11-29 11:19:52 +0100

    Made Spike Prefab
    ...

--- GlitchedOut/Assets/Scripts/Dominik/Harmful.cs
+++ GlitchedOut/Assets/Scripts/Dominik/Harmful.cs
@@ -18,8 +18,9 @@ public class Harmful : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Harm player if he touches this object
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
+            Debug.Log("Player hit spike!");
             //TODO: Reduce HP by 1 or kill player
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
