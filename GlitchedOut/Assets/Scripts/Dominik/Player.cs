e613c2963c58d7eb86a2db80097c29132086a38c
chiptuny22 <121547723+chiptuny22@users.noreply.github.com>
2025-11-29 11:08:14 +0100

    Made Simple Player Script
    Still need to tweak controls and add variable jump height

---
+++ GlitchedOut/Assets/Scripts/Dominik/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundCheck;
+    public float groundCheckRadius= 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Move
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+        }
+
+
+        //Jump
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space)) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+
+        //TODO: Flip sprite
+    }
+}
